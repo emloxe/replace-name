3b881f0ba8bf96e3c1430e242704e4f3f3634a85
Zhang WJ <lemon.zhang@servasoft.com>
2019-07-01 16:51:06 +0800

    完善readme；将异步读取文件的方式改为同步读取；

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const Bagpipe = require('bagpipe'); // 防止过载
 const chalk = require('chalk');
-let bagpipe = new Bagpipe(2);
 const rule = require('./rule');
 
 const rl = readline.createInterface({
@@ -34,7 +32,8 @@ rl.on('line', function(line) {
       }else{
         initMac(line);
       }
-      console.log('处理完成');
+      console.log(chalk.green('处理完成'));
+      process.exit(0);
     }else{
       console.log(chalk.red('error'), '路径不正确，请重新输入');
       rl.prompt();
@@ -42,108 +41,117 @@ rl.on('line', function(line) {
   }
 });
 
-function initMac(line) {
+const initMac = (line) => {
 
 }
 
-function initWin(line){
-  // 根据输入的文件地址，在同级目录创建新的文件夹
-  let newPathArr = line.split('\\');
-  let lastName = newPathArr.pop();
+const initWin = (line) => {
+  const stats = fs.statSync(line);
+  const isFile = stats.isFile(); // 是文件
+  const isDir = stats.isDirectory(); // 是文件夹
+
+  const newPathArr = line.split('\\');
+  const lastName = newPathArr.pop();
   if (!lastName) {
     lastName = newPathArr.pop();
   }
-  newPathArr.push(lastName + '2replace');
-  const newPath = newPathArr.join('\\');
 
-  let isExists = fs.existsSync(newPath);
-  if (!isExists) {
-    fs.mkdir(newPath, function(err) {
+  if(isFile){
+    let extname = path.extname(line); // 后缀名
+    const newPath = newPathArr.join('\\') + '\\';
+
+    dealFile(line, newPath + lastName.split('.')[0] + '2replace' + extname);
+  }
+
+  if(isDir){
+    // 根据输入的文件地址，在同级目录创建新的文件夹 
+    newPathArr.push(lastName + '2replace');
+    const newPath = newPathArr.join('\\');
+    const isExists = fs.existsSync(newPath);
+    if (!isExists) {
+      fs.mkdir(newPath, function(err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+    dealFolder(line, newPath);
+  }
+}
+
+/**
+ * 处理文件
+ * @return {[type]} [description]
+ */
+const dealFile = (filePath, newFilePath, isIgnoreDir = false) => {
+  console.log('Building ' + filePath)
+
+  let extname = path.extname(filePath); // 后缀名
+  let isIgnore = ignoreFile.some(v => extname === v);
+
+  // 判断是否进行匹配操作
+  if (!isIgnore && !isIgnoreDir) {
+    let content = fs.readFileSync(filePath, 'utf-8');
+    content = replaceRule(content);
+    fs.writeFileSync(newFilePath, content, (err) => {
       if (err) {
-        console.error(err);
+        console.log(err);
+      } else {
+        console.log('Finished ' + newFilePath);
+      }
+    });
+  } else {
+    let content = fs.readFileSync(filePath);
+    fs.writeFileSync(newFilePath, content, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Finished ' + newFilePath);
       }
     });
   }
-  readFile(line, newPath);
 }
 
-//文件遍历方法
-function readFile(filePath, newPath, isIgnoreDir = false) {
-  fs.readdir(filePath, function(err, files) {
-    if (err) {
-      console.warn(err)
-    } else {
-      //遍历读取到的文件列表
-      files.forEach(function(filename) {
-        //获取当前文件的绝对路径
-        let filedir = path.join(filePath, filename);
-        let newFilename = replaceRule(filename)
-        let newFilePath = path.join(newPath, newFilename); // 对获取的文件名进行修改
-        // 根据文件路径获取文件信息，返回一个fs.Stats对象
-        fs.stat(filedir, function(eror, stats) {
-          if (eror) {
-            console.warn('获取文件stats失败');
-          } else {
-            let isFile = stats.isFile(); //是文件
-            let isDir = stats.isDirectory(); //是文件夹
-            console.log('Building ' + filedir)
+/**
+ * 处理文件夹
+ * @return {[type]} [description]
+ */
+const dealFolder = (folderPath, newFolderPath, isIgnoreDir = false) => {
+  const files = fs.readdirSync(folderPath); 
+  files.forEach((filename) => {
+    let filedir = path.join(folderPath, filename);
+    let newName = replaceRule(filename)
+    let newPath = path.join(newFolderPath, newName); // 对获取的文件名进行修改
+    // 根据文件路径获取文件信息，返回一个fs.Stats对象
+    const stats = fs.statSync(filedir);
+    const isFile = stats.isFile(); //是文件
+    const isDir = stats.isDirectory(); //是文件夹
 
-            if (isFile) {
-              let extname = path.extname(filedir); // 后缀名
-              let isIgnore = ignoreFile.some(v => extname === v);
 
-              // 判断是否进行匹配操作
-              if (!isIgnore && !isIgnoreDir) {
-                let content = fs.readFileSync(filedir, 'utf-8');
-                content = replaceRule(content);
-                fs.writeFileSync(newFilePath, content, (err) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    console.log('Finished ' + newFilePath);
-                  }
-                });
-              } else {
-                // 防止过载
-                bagpipe.push(function(filedir, newFilePath, callback) {
-                  let readable = fs.createReadStream(filedir); //创建读取流
-                  let writable = fs.createWriteStream(newFilePath); //创建写入流
-                  readable.pipe(writable).on('close', function() {
-                    callback(null);
-                  });
-                }, filedir, newFilePath, function(err) {
-                  // 不会因为文件描述符过多出错
-                  // 妥妥的
-                });
-                console.log('Finished ' + newFilePath);
-
-              }
+    if (isFile) {
+      dealFile(filedir, newPath, isIgnoreDir);
+    }
+    // 如果是文件夹，则在新目录内创建文件夹
+    if (isDir) {
+      let isNotcopy = notcopy.some(v => filename === v);
+      if (!isNotcopy) {
+        // 判断文件夹是否存在，不存在则创建
+        let isExists = fs.existsSync(newPath);
+        if (!isExists) {
+          fs.mkdir(newPath, function(err) {
+            if (err) {
+              console.error(err);
             }
-            // 如果是文件夹，则在新目录内创建文件夹
-            if (isDir) {
-              let isNotcopy = notcopy.some(v => filename === v);
-              if (!isNotcopy) {
-                // 判断文件夹是否存在，不存在则创建
-                let isExists = fs.existsSync(newFilePath);
-                if (!isExists) {
-                  fs.mkdir(newFilePath, function(err) {
-                    if (err) {
-                      console.error(err);
-                    }
-                  });
-                }
-                let isIgnoreDir = ignoreDir.some(v => filename === v);
-                readFile(filedir, newFilePath); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
-              }
-            }
-          }
-        })
-      });
+          });
+        }
+        let isIgnore = ignoreDir.some(v => filename === v);
+        dealFolder(filedir, newPath, isIgnoreDir || isIgnore); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
+      }
     }
   });
 }
 
-function replaceRule(string) {
+const replaceRule = (string) => {
   text.forEach(v => {
     string = string.replace(RegExp(v[0], "g"), v[1])
   })
